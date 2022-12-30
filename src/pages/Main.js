4868bbbb2e31bf0f2cda44eafa78ad27851b3805
Rafael Soares <r.soares2015@outlook.com>
2022-12-30 19:43:04 -0300

    chore: Renderizacao das informacoes mais relevantes

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import useCity from '../hooks/useCity'
+import { roundValues } from '../utils/roundValues';
+import { convertToKm } from '../utils/converter';
 
 export default function Main() {
-  const { handleChange, handleClick, weather } = useCity();
-  console.log(weather);
+  const { handleChange, handleClick, currentWeather } = useCity();
+  const { weather, main, name, wind } = currentWeather;
   return (
     <div>
       <input
@@ -13,6 +15,30 @@ export default function Main() {
         onChange={ handleChange }
       />
       <button type='button' onClick={ handleClick }>Pesquisar</button>
+      {
+        Object.keys(currentWeather).length > 0 && (
+          <div>
+            <div>
+              <h1>{name}</h1>
+              <img src={ `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png` } alt="Icone da previsão do tempo" />
+              <h1>{`${roundValues(main.temp)} °C`}</h1>
+            </div>
+            <div>
+              <p>{`Sensação térmica: ${roundValues(main.feels_like)} °C`}</p>
+            </div>
+            <div>
+              <p>{`Miníma: ${roundValues(main.temp_min)} °C`}</p>
+              <p>{`Maxíma: ${roundValues(main.temp_max)} °C`}</p>
+            </div>
+            <div>
+              <p>{`Umidade relativa do ar: ${main.humidity}%`}</p>
+            </div>
+            <div>
+              <p>{`Velocidade do vento: ${convertToKm(wind.speed)} km/h`}</p>
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
