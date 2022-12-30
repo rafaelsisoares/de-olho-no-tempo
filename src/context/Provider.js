00c7740bde98060ca67d9f233db3ed6996afc7fd
Rafael Soares <r.soares2015@outlook.com>
2022-12-29 21:20:56 -0300

    feat: Criacao do provider

---
+++ src/context/Provider.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import MainContext from './MainContext';
+
+export default function Provider({ children }) {
+  const [city, setCity] = useState();
+  const contextValue = {
+    city,
+    setCity,
+  };
+  return (
+    <MainContext.Provider value={contextValue}>{ children }</MainContext.Provider>
+  )
+}
