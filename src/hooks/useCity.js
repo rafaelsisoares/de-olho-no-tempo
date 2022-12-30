2a3bde83414c21c525e1a493f7290d2721eae241
Rafael Soares <r.soares2015@outlook.com>
2022-12-29 22:05:35 -0300

    chore: Criacao da handleClick e importacao da funcao de requisicao

--- src/hooks/useCity.js
+++ src/hooks/useCity.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
+import requestAPI from "../utils/api";
+
 export default function useCity() {
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState('');
 
   const handleChange = ({ target: { value } }) => {
     const arrCity = value.split(' ');
     const strCity = arrCity.join('+');
-    console.log(strCity);
+    setCity(strCity);
   };
+
+  const handleClick = async () => {
+    const weatherData = await requestAPI(city);
+    setWeather(weatherData);
+  }
 
   return {
     handleChange,
+    handleClick,
+    weather,
   };
 }
