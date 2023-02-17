896405e7e0514abf26f81e43d57a6aea580bdb4f
Rafael Soares <r.soares2015@outlook.com>
2023-02-17 20:10:40 -0300

    refactor: Criacao do componente weatherInfo com codigo movido do Main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,38 +1,18 @@
 import React from 'react'
 
 import Input from '../components/input';
+import WeatherInfo from '../components/weatherInfo';
 import useCity from '../hooks/useCity'
-import { roundValues } from '../utils/roundValues';
-import { convertToKm } from '../utils/converter';
 
 export default function Main() {
   const { handleChange, handleClick, currentWeather } = useCity();
-  const { weather, main, name, wind, cod } = currentWeather;
+  
   return (
     <div>
       <Input handleChange={ handleChange } handleClick={ handleClick }/>
       {
-        Object.keys(currentWeather).length > 0 && cod === 200 && (
-          <div>
-            <div>
-              <h1>{name}</h1>
-              <img src={ `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png` } alt="Icone da previsão do tempo" />
-              <h1>{`${roundValues(main.temp)} °C`}</h1>
-            </div>
-            <div>
-              <p>{`Sensação térmica: ${roundValues(main.feels_like)} °C`}</p>
-            </div>
-            <div>
-              <p>{`Miníma: ${roundValues(main.temp_min)} °C`}</p>
-              <p>{`Maxíma: ${roundValues(main.temp_max)} °C`}</p>
-            </div>
-            <div>
-              <p>{`Umidade relativa do ar: ${main.humidity}%`}</p>
-            </div>
-            <div>
-              <p>{`Velocidade do vento: ${convertToKm(wind.speed)} km/h`}</p>
-            </div>
-          </div>
+        currentWeather.cod === 200 && (
+          <WeatherInfo currentWeather={ currentWeather } />
         )
       }
     </div>
